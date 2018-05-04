222bca5779d14facddc3e8b9898544a10a045ce8
gxx <562606139@qq.com>
2018-05-04 22:22:12 +0800

    0504

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="counter-warp">
+  <div class="container">
+    <div class="userinfo">
+      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
+    </div>
     <wx-menu local="mine"></wx-menu>
-    mine
   </div>
 </template>
 
 <script>
   import menu from "@/components/menu";
   export default {
+    data(){
+      return{
+        userInfo: "",
+      }
+    },
     computed: {
       count () {
       }
@@ -17,15 +24,40 @@
       'wx-menu': menu
     },
 
+    created() {
+      // 调用应用实例的方法获取全局数据
+      this.getUserInfo();
+    },
+
     methods: {
-      increment () {
+      getUserInfo() {
+        // 调用登录接口
+        wx.login({
+          success: () => {
+            wx.getUserInfo({
+              success: res => {
+                this.userInfo = res.userInfo;
+                console.log(res)
+              }
+            });
+          }
+        });
       },
-      decrement () {
-      }
     }
   }
 
 </script>
 <style>
+  .userinfo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .userinfo-avatar {
+    width: 128rpx;
+    height: 128rpx;
+    margin: 20rpx;
+    border-radius: 50%;
+  }
 
 </style>
